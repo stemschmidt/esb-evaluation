bac96c212e3131ee03afa89e3c2e2a06e2b3d9bf
Stefan Schmidt <kontakt@stefanschmidt-embedded.de>
2025-10-06 20:42:12 +0200

    install JLink tools in docker container at startup

---
+++ scripts/setup.sh
@@ -0,0 +1,2 @@
+sudo apt update && sudo apt install -y apt-utils libxcb-render-util0 libxcb-shape0 libxcb-icccm4 libxcb-keysyms1 libxcb-image0 libxcb-image0 libxkbcommon-x11-0
+sudo dpkg -i JLink_Linux_V874a_x86_64.deb
