e7dd2552fa2a3e4a698af24232f54d4903814d7a
Stefan Schmidt <kontakt@stefanschmidt-embedded.de>
2025-10-15 17:31:50 +0200

    working debug environment

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,3 @@
-sudo apt update && sudo apt install -y apt-utils libxcb-render-util0 libxcb-shape0 libxcb-icccm4 libxcb-keysyms1 libxcb-image0 libxcb-image0 libxkbcommon-x11-0
+sudo apt update && sudo apt install -y apt-utils libxcb-render-util0 libxcb-shape0 libxcb-icccm4 libxcb-keysyms1 libxcb-image0 libxcb-image0 libxkbcommon-x11-0 libusb-1.0-0
 sudo dpkg -i JLink_Linux_V874a_x86_64.deb
+sudo dpkg -i nrf-udev_1.0.1-all.deb
