8ddead2b999eaf3e36fef9a91ddd6e18d8d2cde2
Stefan Schmidt <kontakt@stefanschmidt-embedded.de>
2026-07-15 23:05:10 +0200

    send 16 samples per channel within 320µs (~50kHz)

--- esb_ptx/src/main.c
+++ esb_ptx/src/main.c
@@ -29,6 +29,8 @@
 
 LOG_MODULE_REGISTER(esb_ptx, CONFIG_ESB_PTX_APP_LOG_LEVEL);
 
+#define ESB_RF_CHANNEL 10U
+
 #define NUM_SAMPLES 16U
 typedef struct {
     uint16_t left;
@@ -54,7 +56,7 @@ void event_handler(struct esb_evt const* event);
 
 #if defined(CONFIG_CLOCK_CONTROL_NRF)
 int clocks_start(void) {
-    int err;
+    int rc;
     int res;
     struct onoff_manager* clk_mgr;
     struct onoff_client clk_cli;
@@ -67,19 +69,19 @@ int clocks_start(void) {
 
     sys_notify_init_spinwait(&clk_cli.notify);
 
-    err = onoff_request(clk_mgr, &clk_cli);
-    if (err < 0) {
-        LOG_ERR("Clock request failed: %d", err);
-        return err;
+    rc = onoff_request(clk_mgr, &clk_cli);
+    if (rc < 0) {
+        LOG_ERR("Clock request failed: %d", rc);
+        return rc;
     }
 
     do {
-        err = sys_notify_fetch_result(&clk_cli.notify, &res);
-        if (!err && res) {
+        rc = sys_notify_fetch_result(&clk_cli.notify, &res);
+        if (!rc && res) {
             LOG_ERR("Clock could not be started: %d", res);
             return res;
         }
-    } while (err);
+    } while (rc);
 
 #if NRF54L_ERRATA_20_PRESENT
     if (nrf54l_errata_20()) {
@@ -109,15 +111,15 @@ int clocks_start(void) {
 
     sys_notify_init_spinwait(&radio_cli.notify);
 
-    err = nrf_clock_control_request(radio_clk_dev, NULL, &radio_cli);
+    rc = nrf_clock_control_request(radio_clk_dev, NULL, &radio_cli);
 
     do {
-        err = sys_notify_fetch_result(&radio_cli.notify, &res);
-        if (!err && res) {
+        rc = sys_notify_fetch_result(&radio_cli.notify, &res);
+        if (!rc && res) {
             LOG_ERR("Clock could not be started: %d", res);
             return res;
         }
-    } while (err == -EAGAIN);
+    } while (rc == -EAGAIN);
 
     nrf_lrcconf_clock_always_run_force_set(NRF_LRCCONF000, 0, true);
     nrf_lrcconf_task_trigger(NRF_LRCCONF000, NRF_LRCCONF_TASK_CLKSTART_0);
@@ -138,37 +140,27 @@ static int esb_initialize(void) {
     uint8_t base_addr_0[4] = {0xE7, 0xE7, 0xE7, 0xE7};
     uint8_t base_addr_1[4] = {0xC2, 0xC2, 0xC2, 0xC2};
     uint8_t addr_prefix[8] = {0xE7, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8};
-#if 1
-    struct esb_config config = {.protocol = ESB_PROTOCOL_ESB_DPL,
-                                .mode = ESB_MODE_PTX,
-                                .event_handler = event_handler,
-                                .bitrate = ESB_BITRATE_4MBPS,
-                                .crc = ESB_CRC_16BIT,
-                                .tx_output_power = 0,
-                                .retransmit_delay = 0,
-                                .retransmit_count = 0,
-                                .tx_mode = ESB_TXMODE_AUTO,  // ESB_TXMODE_MANUAL,
-                                .payload_length = CONFIG_ESB_MAX_PAYLOAD_LENGTH,
-                                .selective_auto_ack = true,
-                                .use_fast_ramp_up = true};
-#else
-    struct esb_config config = ESB_DEFAULT_CONFIG;
+    struct esb_config esb_config = ESB_DEFAULT_CONFIG;
 
-    config.protocol = ESB_PROTOCOL_ESB_DPL;
-    config.retransmit_delay = 0;
-    config.retransmit_count = 0;
-    config.bitrate = ESB_BITRATE_4MBPS;
-    config.event_handler = event_handler;
-    config.mode = ESB_MODE_PTX;
-    config.selective_auto_ack = true;
-    if (IS_ENABLED(CONFIG_ESB_FAST_SWITCHING)) {
-        config.use_fast_ramp_up = true;
-    }
+    /* PTX Modus: nur senden */
+    esb_config.protocol = ESB_PROTOCOL_ESB_DPL;
+    esb_config.mode = ESB_MODE_PTX;
+    esb_config.bitrate = ESB_BITRATE_4MBPS;
+    esb_config.crc = ESB_CRC_16BIT;
 
-#endif
-    rc = esb_init(&config);
+    /* Kein ACK, keine Retransmits → maximaler Durchsatz */
+    esb_config.retransmit_count = 0;
+    esb_config.retransmit_delay = 0;
+    esb_config.selective_auto_ack = true; /* ACK per Paket steuerbar */
 
+    /* TX FIFO sofort leeren */
+    esb_config.tx_mode = ESB_TXMODE_AUTO;
+
+    esb_config.event_handler = event_handler;
+
+    rc = esb_init(&esb_config);
     if (rc) {
+        LOG_ERR("esb_init: %d", rc);
         return rc;
     }
 
@@ -187,18 +179,19 @@ static int esb_initialize(void) {
         return rc;
     }
 
-    esb_flush_tx();
+    rc = esb_set_rf_channel(ESB_RF_CHANNEL);
+    if (rc) {
+        return rc;
+    }
 
     return 0;
 }
 
 void event_handler(struct esb_evt const* event) {
-    dk_set_led(0, 0);
-
     switch (event->evt_id) {
         case ESB_EVENT_TX_SUCCESS:
             packets_send++;
-            k_sem_give(&tx_sem);
+            dk_set_led(0, 0);
             break;
         case ESB_EVENT_TX_FAILED:
             LOG_DBG("TX FAILED EVENT");
@@ -209,36 +202,31 @@ void event_handler(struct esb_evt const* event) {
     }
 }
 
-static void sample_handler(struct k_timer* timer) {
-    dk_set_led(0, 1);
-    int rc = esb_start_tx();
-    if (rc != 0) {
-        LOG_ERR("esb_start_tx returned %d", rc);
-    }
-}
+static void sample_handler(struct k_timer* timer) { k_sem_give(&tx_sem); }
 
 int main(void) {
-    int err;
-    bool timer_started = false;
+    int rc;
+    uint16_t counter = 0;
     audio_message_t message = {0};
 
     LOG_INF("Enhanced ShockBurst ptx sample. sample message size: %d bytes", sizeof(audio_message_t));
 
-    err = clocks_start();
-    if (err) {
+    rc = clocks_start();
+    if (rc) {
         return 0;
     }
 
-    err = dk_leds_init();
-    if (err) {
-        LOG_ERR("LEDs initialization failed, err %d", err);
+    rc = dk_leds_init();
+    if (rc) {
+        LOG_ERR("LEDs initialization failed, rc %d", rc);
         return 0;
     }
     dk_set_led(0, 0);
+    dk_set_led(1, 0);
 
-    err = esb_initialize();
-    if (err) {
-        LOG_ERR("ESB initialization failed, err %d", err);
+    rc = esb_initialize();
+    if (rc) {
+        LOG_ERR("ESB initialization failed, rc %d", rc);
         return 0;
     }
 
@@ -249,25 +237,26 @@ int main(void) {
     LOG_INF("Initialization complete");
     LOG_INF("Sending test packet");
 
-    while (1) {
-        k_sem_take(&tx_sem, K_FOREVER);
-        for (int i = 0; i < NUM_SAMPLES; i++) {
-            message.samples[i].left = i;
-            message.samples[i].right = NUM_SAMPLES - i;
-        }
+    k_timer_start(&sample_timer, K_USEC(320), K_USEC(320));
 
+    while (1) {
+        /* Prepare package. */
+        for (int i = 0; i < NUM_SAMPLES; i++) {
+            message.samples[i].left = counter;
+            message.samples[i].right = counter;
+            counter++;
+        }
+        message.sequence_number++;
+
+        k_sem_take(&tx_sem, K_FOREVER);
+
+        /* Send package. */
         memcpy(tx_payload.data, &message, sizeof(audio_message_t));
         int rc = esb_write_payload(&tx_payload);
         if (rc) {
-            LOG_ERR("Payload write failed, rc %d", rc);
-        }
-        dk_set_led(0, 1);
-
-        message.sequence_number++;
-
-        if (0 && timer_started == false) {
-            k_timer_start(&sample_timer, K_USEC(333), K_USEC(333));
-            timer_started = true;
+            dk_set_led(1, 1);
+        } else {
+            dk_set_led(0, 1);
         }
     }
 }
